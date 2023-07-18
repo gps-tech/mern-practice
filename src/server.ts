e4475ea452a14ca1182b6a9fc2a22c48c65a8ae4
Gianmarco Pow-Sang <hello@gpstech.io>
2023-07-17 23:00:03 -0700

    installed envalidate

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5001;
 
 //endpoint for http GET request
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(port, () => {
-  console.log("Server running on port: " + port);
-});
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
